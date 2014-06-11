d0c27bd8b82ce4c9d54521e9f32d4d2754af13b2
Akshit Malhotra <aks.m.18@gmail.com>
2014-06-11 16:07:57 +0200

    adding TestCases for "Order a Meal"

--- CookForMe/test/ApplicationTest.java
+++ CookForMe/test/ApplicationTest.java
@@ -1,4 +1,8 @@
+import java.util.List;
+
 import org.junit.*;
+
+import play.Logger;
 import play.test.*;
 import play.mvc.*;
 import play.mvc.Http.*;
@@ -12,6 +16,29 @@ public class ApplicationTest extends FunctionalTest {
         assertIsOk(response);
         assertContentType("text/html", response);
         assertCharset(play.Play.defaultWebEncoding, response);
+    }
+    
+    @Test
+    public void addToBasketTest(){
+    	Item item = new Item("Dal2", "Makhani", 10); //name, description, price	
+    	Basket b = new Basket("akshit"); //userID
+    	b.addItem(item);
+    	//b.save();
+    	
+    	List<Item> items = Item.find("order by name").from(1).fetch(100);
+		
+		Logger.debug("In index()"+items.size());
+		for(Item item1:items){
+			Logger.debug(item1.name+",");
+		}
+    	
+    	
+    	//Item retrieved = Item.findById(item.id);	
+    	//assertEquals("Dal", retrieved.name);
+    	
+//		assertEquals(new Integer(2013), retrieved.year);
+    	
+    	
     }
     
 }
